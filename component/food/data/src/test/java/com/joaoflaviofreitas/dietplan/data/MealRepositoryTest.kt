375da537bc613724da11f504a6448b195cc6c7d4
joaoflaviofreitas <joaoflaviofreitas@gmail.com>
2023-06-03 15:21:13 -0300

    add more tests

--- component/food/data/src/test/java/com/joaoflaviofreitas/dietplan/data/MealRepositoryTest.kt
+++ component/food/data/src/test/java/com/joaoflaviofreitas/dietplan/data/MealRepositoryTest.kt
@@ -4,6 +4,9 @@ import com.joaoflaviofreitas.dietplan.component.food.data.local.Dao
 import com.joaoflaviofreitas.dietplan.component.food.data.remote.Api
 import com.joaoflaviofreitas.dietplan.component.food.data.repository.MealRepositoryImpl
 import com.joaoflaviofreitas.dietplan.component.food.domain.model.AchievedGoal
+import com.joaoflaviofreitas.dietplan.component.food.domain.model.Meal
+import com.joaoflaviofreitas.dietplan.component.food.domain.model.ProductResponse
+import com.joaoflaviofreitas.dietplan.component.food.domain.model.RequestFood
 import com.joaoflaviofreitas.dietplan.component.food.domain.repository.MealRepository
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -13,6 +16,7 @@ import io.mockk.runs
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Response
 
 class MealRepositoryTest {
 
@@ -51,6 +55,31 @@ class MealRepositoryTest {
 
             coVerify { dao.updateAchievedGoal(achievedGoal) }
             assert(!result)
+        }
+    }
+
+    @Test
+    fun `Verify if Api returns nutrients`() {
+        val ingredients = RequestFood("Rice", 1.0, "kg")
+        val mockedResponse: Response<ProductResponse> = mockk()
+        runBlocking {
+            coEvery { api.getFoodInfo(any()) } returns mockedResponse
+
+            val result = repository.getNutrientsByApi(ingredients)
+
+            assert(result == mockedResponse)
+        }
+    }
+
+    @Test
+    fun `Verify if save meal in database is success`() {
+        val meal = Meal(2, "Rice", protein = 200.2)
+        runBlocking {
+            coEvery { dao.insertAll(any()) } just runs
+
+            repository.saveMealInDatabase(meal)
+
+            coVerify { dao.insertAll(meal) }
         }
     }
 }
