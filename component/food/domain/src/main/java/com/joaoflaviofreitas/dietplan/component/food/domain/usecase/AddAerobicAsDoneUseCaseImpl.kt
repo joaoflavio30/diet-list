b19417b9d44f3cdb059e4216bb490bf292cb2433
joaoflaviofreitas <joaoflaviofreitas@gmail.com>
2023-06-05 12:05:07 -0300

    change addAerobicUseCase to internal class

--- component/food/domain/src/main/java/com/joaoflaviofreitas/dietplan/component/food/domain/usecase/AddAerobicAsDoneUseCaseImpl.kt
+++ component/food/domain/src/main/java/com/joaoflaviofreitas/dietplan/component/food/domain/usecase/AddAerobicAsDoneUseCaseImpl.kt
@@ -1,9 +1,8 @@
 package com.joaoflaviofreitas.dietplan.component.food.domain.usecase
 
 import com.joaoflaviofreitas.dietplan.component.food.domain.repository.MealRepository
-import kotlinx.coroutines.flow.last
 
-class AddAerobicAsDoneUseCaseImpl(private val repository: MealRepository): AddAerobicAsDoneUseCase {
+internal class AddAerobicAsDoneUseCaseImpl(private val repository: MealRepository): AddAerobicAsDoneUseCase {
     override suspend fun execute(userEmail: String): Boolean {
         return try {
             val achievedGoal = repository.getAchievedGoal(userEmail)
